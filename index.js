9c34961e2d1230f0cac4ca17fe3c87b2a2408b23
Safisyx <safisyx@gmail.com>
2018-03-11 20:17:17 +0100

    Initialize express project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+app.listen(4001, () => console.log('Express API listening on port 4001'))
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+  next()
+})
