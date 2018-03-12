58e19bff13c1784c424d174d2c79d8266b67a278
Safisyx <safisyx@gmail.com>
2018-03-12 01:37:57 +0100

    Add login end-point

--- users/router.js
+++ users/router.js
@@ -33,5 +33,31 @@ router.post('/users', (req, res) => {
 
 })
 
+router.post('/logins', (req,res) => {
+  User
+  	.findOne({
+  		where: {
+  			email: req.body.email
+  		}
+  	})
+  	.then(entity => {
+  		if (bcrypt.compareSync(req.body.password, entity.password)) {
+  			res.send({
+  				jwt: sign(entity.id)
+  			})
+  		}
+  		else {
+  			res.status(400).send({
+  				message: 'Password was incorrect'
+  			})
+  		}
+  	})
+  	.catch(err => {
+  		console.error(err)
+  		res.status(500).send({
+  			message: 'Something went wrong'
+  		})
+  	})
+})
 
 module.exports = router
